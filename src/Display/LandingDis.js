ac34e619f373ae47e80b9fb7a42b7ddf52489856
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-02-08 13:23:58 -0600

    landing styling complete

--- src/Display/LandingDis.js
+++ src/Display/LandingDis.js
@@ -5,31 +5,34 @@ import '../Style/landing.css';
 export default function LandingDis() {
 
     return (
-        <div id='landing_view'>
+        <div id='back_img'>
+            <div id='landing_view'>
             
-            <div id='welcome_header'>
-                Hello, my name is Ryan Harris. <br />
-                <hr style={{width: '90%'}}/>
-                Welcome to my Personal Portfolio Website!
-            </div>
-            <div id='welcome_message'>
-                Here we will see some of my personal projects and a little information about me. <br />
-                Thank you for visiting.
-            </div>
+                <div id='welcome_header'>
+                    Hello, my name is Ryan Harris. <br />
+                    <hr style={{width: '85%'}}/>
+                    Welcome to my Personal Portfolio Website!
+                </div>
+                <div id='welcome_message'>
+                    Here we will see some of my personal projects and a little information about me. <br />
+                    Thank you for visiting.
+                </div>
 
-            <div id='btn_group'>
-                <Link to={'/login'}>
-                    <button id='login_btn'>
-                        Login
-                    </button>
-                </Link>
-                <Link to={'/register'}>
-                    <button id='register_btn'>
-                        Register
-                    </button>
-                </Link>
-            </div>
+                <div id='btn_group'>
+                    <Link to={'/login'}>
+                        <button id='login_btn'>
+                            Login
+                        </button>
+                    </Link>
+                    <Link to={'/register'}>
+                        <button id='register_btn'>
+                            Register
+                        </button>
+                    </Link>
+                </div>
 
-      </div> 
+            </div>
+        </div>
+         
   )
 }
