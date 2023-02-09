fec6e76d33a186644661ccf73cea6acf6cf39dd8
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-02-09 15:49:37 -0600

    registration form completed with validation

--- src/Display/RegisterDis.js
+++ src/Display/RegisterDis.js
@@ -1,3 +1,4 @@
+import '../Style/register.css';
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -53,32 +54,116 @@ export default function RegisterDis() {
   }, [])
 
   return (
-    <div id='register_view'>
-      <form onSubmit={submitForm}>
+    <div>
+      <div id='register_view'>
+        <div id='register_directions'>
+          <div id='register_header'>Registration</div>
+          <div id='register_message'>Please fill out the below fields.</div>
+        </div>
+        <form onSubmit={submitForm}>
 
-        <input type='text' className='form_field' placeholder='First Name' value={firstName}
-          onChange={e => setFirstName(e.target.value)} required={true} />
-        
-        <input type='text' className='form_field' placeholder='Last Name' value={lastName}
-          onChange={e => setLastName(e.target.value)} required={true} />
-        
-        <input type='email' className='form_field' placeholder='Email' value={email}
-          onChange={e => setEmail(e.target.value)} required={true} />
-        
-        <input type='number' className='form_field' placeholder='Age' value={age}
-          onChange={e => setAge(e.target.value)} required={true} />
-        
-        <input type='text' className='form_field' placeholder='Username' value={userName}
-          onChange={e => setUserName(e.target.value)} required={true} />
-        
-        <input type='password' className='form_field' placeholder='Password' value={password}
-          onChange={e => setPassword(e.target.value)} required={true} />
-        
-        <input type='password' className='form_field' placeholder='Confirm Password' value={confPassword}
-          onChange={e => setConfPassword(e.target.value)} required={true} />
-        
-        <button type='submit'>Complete Registration</button>
-      </form>
+          <input
+            type='text'
+            className='form_field'
+            placeholder='First Name'
+            value={firstName}
+            required={true}
+            style={ firstName.length > 0
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setFirstName(e.target.value)} />
+          
+          <input
+            type='text'
+            className='form_field'
+            placeholder='Last Name'
+            value={lastName}
+            required={true}
+            style={ lastName.length > 0
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setLastName(e.target.value)} />
+          
+          <input
+            type='email'
+            className='form_field'
+            placeholder='Email'
+            value={email}
+            required={true}
+            style={email.length > 0
+              && email.includes('@')
+              && email.includes('.com')
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setEmail(e.target.value)} />
+          
+          <input
+            type='number'
+            className='form_field'
+            placeholder='Age'
+            value={age}
+            required={true}
+            style={ age.length > 0
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setAge(e.target.value)} />
+          
+          <input
+            type='text'
+            className='form_field'
+            placeholder='Username'
+            value={userName}
+            required={true}
+            style={ userName.length > 0
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setUserName(e.target.value)} />
+          
+          <input
+            type='password'
+            className='form_field'
+            placeholder='Password'
+            value={password}
+            required={true}
+            style={ password.length > 0
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setPassword(e.target.value)} />
+          
+          <input
+            type='password'
+            className='form_field'
+            placeholder='Confirm Password'
+            value={confPassword}
+            required={true}
+            style={ confPassword.length > 0
+              && password === confPassword
+              ? { borderBottom: '2px solid blue' }
+              : { borderBottom: '2px solid red'}}
+            onChange={e => setConfPassword(e.target.value)} />
+          
+          { password.length > 0
+            &&
+            confPassword.length > 0
+            &&
+            password !== confPassword
+            ?
+            <div
+              style={{
+                width: '450px',
+                margin: 'auto',
+                textAlign: 'start',
+                fontSize: '12px'
+              }}>
+              Your passwords do not currently match.
+            </div>
+            :
+            <div></div>}
+          
+          <button id='form_btn' type='submit'>Complete Registration</button>
+
+        </form>
+      </div>
     </div>
   )
 }
