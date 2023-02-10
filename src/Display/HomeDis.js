f66eea4a3711892cebe00ec18df6ae0fbdc17917
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-02-10 14:53:04 -0600

    login page started

---
+++ src/Display/HomeDis.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeDis() {
+  return (
+    <div>HomeDis</div>
+  )
+}
--- src/Display/LoginDis.js
+++ src/Display/LoginDis.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import '../Style/login.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoremIpsum, { loremIpsum } from 'react-lorem-ipsum';
 
 export default function LoginDis() {
 
   const [users, setUsers] = useState(null);
+  const [status, setStatus] = useState(false);
+
+  const statusChange = (e) => {
+    if (status === true) {
+      setStatus(false);
+    } else {
+      setStatus(true);
+    }
+  }
 
   useEffect(() => {
     fetch('api/users')
@@ -14,6 +26,104 @@ export default function LoginDis() {
   }, [])
 
   return (
-    <div>LoginDis</div>
+    <div>
+
+      <div id='login_view'>
+        <div id='login_directions'>
+          <div>Hello & Welcome!</div>
+        </div>
+        <form>
+
+          <input
+            type='text'
+            className='login_field'
+            placeholder='Username'
+            required={true} />
+
+          <input
+            type='password'
+            className='login_field'
+            placeholder='Password'
+            required={true} />
+
+          <button id='login_bttn' type='submit'>
+              Login
+          </button>
+
+          <div id='reg_redirect'>
+            Don't have an account?
+            <div>
+              <a href='/register'>
+                Register Here
+              </a>
+            </div>
+          </div>
+
+          <div id='pass_change'>
+            Forgot your password?
+            <div>
+              <button type='button' onClick={statusChange}>
+                Change Status
+              </button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+
+      { status === true
+        ?
+        <div id='password_view'>
+          <div id='password_directions'>
+            Reset your Password!
+          </div>
+          <form>
+
+            <input
+              type='text'
+              className='password_field'
+              placeholder='Username'
+              required={true} />
+
+            <input
+              type='text'
+              className='password_field'
+              placeholder='Email'
+              required={true} />
+            
+            <input
+              type='password'
+              className='password_field'
+              placeholder='New Password'
+              required={true} />
+            
+            <input
+              type='password'
+              className='password_field'
+              placeholder='Confirm New Password'
+              required={true} />
+            
+            <button id='passReset_btn'
+              type='submit'>
+                Confirm
+            </button>
+
+            <button id='passCancel_btn'
+              type='cancel'
+              onClick={e => setStatus(false)}>
+                Cancel
+            </button>
+            
+          </form>
+        </div>
+        :
+        <div id='loginInfo_view'>
+          <div id='loginInfo_text'>
+            <LoremIpsum p={2} />
+          </div>
+        </div>
+      }
+
+    </div>
   )
 }
