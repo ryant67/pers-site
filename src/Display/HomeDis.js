1079c4c58b6d283b232a0d10d008a682669211c2
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-02-16 17:10:44 -0600

    added access check

--- src/Display/HomeDis.js
+++ src/Display/HomeDis.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomeDis() {
 
+  const navigate = useNavigate();
+
+  const accessCheck = () => {
+    localStorage.getItem('userName') === null 
+      ? navigate('/login')
+      : navigate('/home')
+  }
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+
+    localStorage.clear()
+
+    navigate('/login')
+  }
   
+  useEffect(() => {
+    accessCheck()
+  }, [])
 
   return (
-    <div>Hello {localStorage.firstName} { localStorage.lastName }</div>
+    <div>
+      <div>Hello {localStorage.firstName} {localStorage.lastName}</div>
+      <buton id='home_btn' type='button' onClick={handleLogout}>
+          Logout
+      </buton>
+    </div>
+    
   )
 }
