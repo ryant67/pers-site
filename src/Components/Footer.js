a82f9cf8b9ea076139de2c53ef66f99eee09f5e7
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-02-20 18:50:34 -0600

    footer classifications

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,35 +7,35 @@ export default function Footer() {
   return (
     <div>
       <div id='footer_view'>
-        <ul>
-          <li> | </li>
-          <li>
+        <ul id='footer_list'>
+          <li className='footer_item'> | </li>
+          <li className='footer_item'>
             <a target='_blank'
               href='https://www.linkedin.com/in/ryantharris/'>
               LinkedIn
             </a>
           </li>
-          <li> | </li>
-          <li>
+          <li className='footer_item'> | </li>
+          <li className='footer_item'>
             <a target='_blank'
               href='https://github.com/ryant67'>
               GitHub
             </a>
           </li>
-          <li> | </li>
-          <li>
+          <li className='footer_item'> | </li>
+          <li className='footer_item'>
             <a target='_blank'
               href='https://alumni.codeup.com/students/1657'>
               Alumni Portal
             </a>
           </li>
-          <li> | </li>
-          <li>
+          <li className='footer_item'> | </li>
+          <li className='footer_item'>
             <a href='/about'>
               About Me
             </a>
           </li>
-          <li> | </li>
+          <li className='footer_item'> | </li>
         </ul>
         <div id='top'>
           <a href='#top'>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,58 @@
 import '../Style/navbar.css';
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
+
+  const [dropStatus, setDropStatus] = useState(false);
+
+  const handleDropdown = (e) => {
+    e.preventDefault()
+    if (dropStatus === false) {
+      setDropStatus(true)
+    } else {
+      setDropStatus(false)
+    }
+  }
+
   return (
     <div>
       <div id='nav_view'>
-        <div id='top'></div>
+        <div
+          id='top'
+          style={{height: '0', width: '0'}}></div>
+        <div id='nav_dropdown'>
+          <ul>
+            <li>Ryan Harris</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>Software Developer</li>
+          </ul>
+        </div>
+
+
+
+
+
+
+        {/* <div id='nav_dropdown'>
+          <button
+            type='button'
+            onClick={handleDropdown}>
+            Project Selection
+          </button>
+          {dropStatus === true
+            ?
+            <div>
+              <ul>
+                
+              </ul>
+            </div>
+            :
+            <div></div>}
+        </div> */}
+        
       </div>
     </div>
   )
