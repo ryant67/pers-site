c222341f71cc73e97e426c612e6358e49c7fc2a6
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-03-04 23:40:05 -0600

    error/404 page created

--- src/Display/ErrorDis.js
+++ src/Display/ErrorDis.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../Style/error.css';
 
 export default function ErrorDis() {
   return (
-    <div>ErrorDis</div>
+    <div id='whole_error'>
+      <div id='error_text'>
+        <div id='four_header'>Error 404!</div>
+        <div id='four_body'>
+          You seem to have wandered to a place that does not exist!!
+        </div>
+        <div>
+          <i 
+            style={{margin: '30px 0'}}
+            class="fa-solid fa-10x fa-circle-exclamation"></i>
+        </div>
+      </div>
+    </div>
   )
 }
