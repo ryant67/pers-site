6fa8537f7574b5910246120407218835bb255f66
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-03-05 00:41:25 -0600

    added color change functionality to error page

--- src/Display/ErrorDis.js
+++ src/Display/ErrorDis.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import '../Style/error.css';
+import { useEffect, useState } from 'react';
 
 export default function ErrorDis() {
+
+  const [color, setColor] = useState('lightseagreen');
+
+  const colorChange = (e) => {
+    e.preventDefault();
+
+    if (color === 'palevioletred') {
+      setColor('lightgreen');
+    } else if (color === 'lightgreen') {
+      setColor('lightcoral');
+    } else if (color === 'lightcoral') {
+      setColor('lightseagreen');
+    } else if (color === 'lightseagreen') {
+      setColor('lightskyblue');
+    } else if (color === 'lightskyblue') {
+      setColor('palevioletred');
+    }
+  }
+
   return (
-    <div id='whole_error'>
+    <div
+      style={{backgroundColor: color}}
+      id='whole_error'>
       <div id='error_text'>
         <div id='four_header'>Error 404!</div>
         <div id='four_body'>
           Apologies, but you seem to have wandered to a place that does not exist!!
         </div>
         <div>
-          <i 
-            style={{margin: '30px 0'}}
-            class="fa-solid fa-10x fa-circle-exclamation"></i>
+          <i
+            onClick={colorChange}  
+            style={{ margin: '30px 0 10px 0' }}
+            id='exclamation'
+            className="fa-solid fa-10x fa-circle-exclamation"></i>
         </div>
+        pssst, click me...
       </div>
     </div>
   )
