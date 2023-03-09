758ee88ca28ba772c3280ee60cfc451e1b8309dc
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-03-09 16:16:23 -0600

    basic to be determined page created and implemented

--- src/Display/ErrorDis.js
+++ src/Display/ErrorDis.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Style/error.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function ErrorDis() {
 
--- src/Display/TbdDis.js
+++ src/Display/TbdDis.js
@@ -2,7 +2,20 @@ import '../Style/tbd.css';
 import React from 'react';
 
 export default function TbdDis() {
-  return (
-    <div>TbdDis</div>
+    return (
+        <div id='whole_tbd'>
+            <div id='tbd_text'>
+                <div id='tbd_header'>
+                    Hey {localStorage.firstName}! <br />
+                    ( {localStorage.userName} )
+                </div>
+                <div id='tbd_message'>
+                    You've found a page that is currently in
+                    the works. I'm sure if you came back soon there
+                    would be something pretty cool here to check out! <br />
+                    Thank you for visiting!
+                </div>
+            </div>
+        </div>
   )
 }
