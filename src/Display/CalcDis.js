795a7a2cdef13a1a706e29047b9392a827bb39dc
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-03-09 17:47:55 -0600

    calculator design and layout complete no functionality yet

--- src/Display/CalcDis.js
+++ src/Display/CalcDis.js
@@ -1,7 +1,52 @@
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import '../Style/calc.css';
 import React from 'react'
 
 export default function CalcDis() {
   return (
-    <div>CalcDis</div>
+    <div>
+
+      <Navbar />
+
+      <div id='calc_dis'>
+        <div id='calc_text'>
+          <div id='calc_header'>
+            Calculator Project
+          </div>
+        </div>
+        <div id='whole_calc'>
+          <div id='calc_outline'>
+            <div id='text_field'></div>
+            <div id='btn_field'>
+              <div className='operator_btn'>C</div>
+              <div className='operator_btn'>+ / -</div>
+              <div className='operator_btn'>%</div>
+              <div className='operator_btn'>
+                <i class="fa-solid fa-divide"></i>
+              </div>
+              <div className='number_btn'>7</div>
+              <div className='number_btn'>8</div>
+              <div className='number_btn'>9</div>
+              <div className='operator_btn'>x</div>
+              <div className='number_btn'>4</div>
+              <div className='number_btn'>5</div>
+              <div className='number_btn'>6</div>
+              <div className='operator_btn'>-</div>
+              <div className='number_btn'>1</div>
+              <div className='number_btn'>2</div>
+              <div className='number_btn'>3</div>
+              <div className='operator_btn'>+</div>
+              <div id='big_btn'>0</div>
+              <div className='number_btn'>.</div>
+              <div className='operator_btn'>=</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+
+    </div>
   )
 }
