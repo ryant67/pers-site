30dbd8048e8afc41662df98e0aea2aec97a955d5
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-03-09 16:22:48 -0600

    nav and footer added to tbd page

--- src/Display/TbdDis.js
+++ src/Display/TbdDis.js
@@ -1,21 +1,28 @@
 import '../Style/tbd.css';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 import React from 'react';
 
 export default function TbdDis() {
     return (
-        <div id='whole_tbd'>
-            <div id='tbd_text'>
-                <div id='tbd_header'>
-                    Hey {localStorage.firstName}! <br />
-                    ( {localStorage.userName} )
-                </div>
-                <div id='tbd_message'>
-                    You've found a page that is currently in
-                    the works. I'm sure if you came back soon there
-                    would be something pretty cool here to check out! <br />
-                    Thank you for visiting!
+        <div>
+            <Navbar />
+            <div id='whole_tbd'>
+                <div id='tbd_text'>
+                    <div id='tbd_header'>
+                        Hey {localStorage.firstName}! <br />
+                        ( {localStorage.userName} )
+                    </div>
+                    <div id='tbd_message'>
+                        You've found a page that is currently in
+                        the works. I'm sure if you came back soon there
+                        would be something pretty cool here to check out! <br />
+                        Thank you for visiting!
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
+        
   )
 }
